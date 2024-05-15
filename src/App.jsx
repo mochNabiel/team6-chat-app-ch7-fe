6bdb613aba9f88990ae6c564637a6a3b532e11a7
dvlboo <kuhhhckr@gmail.com>
2024-05-15 19:28:06 +0700

    add routes files

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,6 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
+import router from "./routes"
 
-import ChatPage from "./pages/ChatPage"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import ProfilePage from "./pages/ProfilePage"
-import RegisterPage from "./pages/RegisterPage"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <HomePage />
-      </>
-    )
-  },
-  {
-    path: "/chat",
-    element: (
-      <>
-        <ChatPage />
-      </>
-    )
-  },
-  {
-    path: "/profile",
-    element: (
-      <>
-        <ProfilePage />
-      </>
-    )
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <LoginPage />
-      </>
-    )
-  },
-  {
-    path: "/register",
-    element: (
-      <>
-        <RegisterPage />
-      </>
-    )
-  },
-
-])
-
-function App() {
+export default function App() {
   return <RouterProvider router={router} />
 }
-
-export default App
