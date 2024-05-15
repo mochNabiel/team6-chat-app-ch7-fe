52e7d20936a98c6facc957cace47f2c3a2c1f1e8
Mochammad_Nabiel <113111900+mochNabiel@users.noreply.github.com>
2024-05-15 21:49:36 +0700

    Dev nabiel (#5)
    * add loginpage and registerpage
    
    * add : google login, profile pages, protected route, redux & some frontend functionalities
    
    * add : git ignore for env
    
    * Remove .env from tracking and update .gitignore
    
    * fix login
    
    * delete role & unused bracket
    
    * delete bracket in App.jsx
    
    * fix profile page
    
    * add initialize for chat page
    
    ---------
    
    Co-authored-by: aufarabul <aufarabul@gmail.com>

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,4 +1,85 @@
+import { useEffect, useState } from "react"
+import io from "socket.io-client"
+import ScrollToBottom from "react-scroll-to-bottom"
+import { IoSend } from "react-icons/io5"
+
+import "../assets/css/chat.css"
+
+const socket = io("http://localhost:3004")
+
 const ChatPage = () => {
-  return <div>Chat</div>
+  const [currentMessage, setCurrentMessage] = useState("")
+  const [messageList, setMessageList] = useState([])
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      }
+
+      await socket.emit("send_message", messageData)
+      setMessageList((list) => [...list, messageData])
+      setCurrentMessage("")
+    }
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data])
+    })
+  }, [socket])
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="chat-window">
+        <div className="chat-header">
+          <p>Live Chat</p>
+        </div>
+        <div className="chat-body">
+          <ScrollToBottom className="message-container">
+            {messageList.map((messageContent) => {
+              return (
+                <div
+                  className="message"
+                  id={username === messageContent.author ? "you" : "other"}
+                >
+                  <div>
+                    <div className="message-content">
+                      <p>{messageContent.message}</p>
+                    </div>
+                    <div className="message-meta">
+                      <p id="time">{messageContent.time}</p>
+                      <p id="author">{messageContent.author}</p>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </ScrollToBottom>
+        </div>
+        <div className="chat-footer">
+          <input
+            type="text"
+            value={currentMessage}
+            placeholder="Hey..."
+            onChange={(event) => {
+              setCurrentMessage(event.target.value)
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && sendMessage()
+            }}
+          />
+          <button onClick={sendMessage}>
+            <IoSend />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default ChatPage
