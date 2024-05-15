4f8c6bf8d0362938d4b7728b0c31d4d64c113843
aufarabul <136091204+aufarabul@users.noreply.github.com>
2024-05-15 11:49:05 +0700

    add loginpage and registerpage (#1)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,18 @@
+import LoginComponent from "../components/Login"
+import { Row, Col, Card } from "react-bootstrap"
+
 const LoginPage = () => {
-  return <div>Login</div>
+  return (
+    <Row>
+      <Col md={6} className="offset-md-3 mt-5 ">
+        <Card className="shadow p-2 mb-5 bg-body-primary rounded">
+          <Card.Header>Login</Card.Header>
+          <Card.Body>
+            <LoginComponent />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 export default LoginPage
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,4 +1,20 @@
+import RegisterComponent from "../components/Register"
+import { Row, Col, Card } from "react-bootstrap"
+
 const RegisterPage = () => {
-  return <div>Register</div>
+  return (
+    <>
+      <Row>
+        <Col md={6} className="offset-md-3 mt-5">
+          <Card className="shadow p-3 mb-5 bg-body-primary rounded">
+            <Card.Header>Register</Card.Header>
+            <Card.Body>
+              <RegisterComponent />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 export default RegisterPage
