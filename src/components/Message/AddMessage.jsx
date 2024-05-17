3b68d7d1d637eb6d7af70c056a356d56ece530b7
Kukuh Cokro Wibowo <92429096+dvlboo@users.noreply.github.com>
2024-05-17 07:16:15 +0700

    logic chatting (#10)

--- src/components/Message/AddMessage.jsx
+++ src/components/Message/AddMessage.jsx
@@ -1,63 +1,49 @@
-import { useState } from "react"
-import PropTypes from "prop-types"
-import FloatingLabel from "react-bootstrap/FloatingLabel"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import { toast } from "react-toastify"
-import { useDispatch } from "react-redux"
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { MessageInput } from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";  
 
-import { createNewMessage } from "../../redux/actions/message"
+import { createNewMessage } from "../../redux/actions/message";
 
 function AddMessage({ socket }) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [addMessage, setAddMessage] = useState("");
 
-  const [addMessage, setAddMessage] = useState("")
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    if (addMessage === "") {
-      toast.error("Message can not be empty!")
-      return
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSubmit();
     }
+  };
 
-    dispatch(createNewMessage(addMessage))
-
-    setAddMessage("")
-  }
+  const handleSubmit = () => {
+    if (addMessage.trim() === "") {
+      toast.error("Message can not be empty!");
+      return;
+    }
+    dispatch(createNewMessage(addMessage));
+    setAddMessage("");
+  };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Row className="g-2 mt-4">
-        <Col md>
-          <FloatingLabel controlId="floatingInputGrid" label="Add Message">
-            <Form.Control
-              type="text"
-              placeholder="Add message"
-              value={addMessage}
-              onChange={(e) => {
-                setAddMessage(e.target.value)
-                socket.emit("typing")
-              }}
-            />
-          </FloatingLabel>
-        </Col>
-        <Col md>
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="submit" size="lg">
-              Add Message
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    </Form>
-  )
+    <MessageInput
+      placeholder="Add message"
+      value={addMessage}
+      onChange={(value) => {
+        setAddMessage(value);
+        socket.emit("typing");
+      }}
+      style={{ flex: 1 }}
+      onKeyDown={handleKeyDown}
+      onSubmit={handleSubmit}
+    />
+  );
 }
 
 AddMessage.propTypes = {
   socket: PropTypes.any,
-}
+};
 
-export default AddMessage
+export default AddMessage;
--- src/components/Message/MessageItem.jsx
+++ src/components/Message/MessageItem.jsx
@@ -1,46 +1,45 @@
-import Card from "react-bootstrap/Card"
-import PropTypes from "prop-types"
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
-import { getProfile } from "../../redux/actions/auth"
-import { Image } from "react-bootstrap"
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getProfile } from "../../redux/actions/auth";
+import { Avatar, Message } from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
 function MessageItem({ data }) {
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
-  const formatNumber = (number) => number.toString().padStart(2, "0")
-  const date = new Date(data.createdAt)
-  const hours = formatNumber(date.getHours())
-  const minutes = formatNumber(date.getMinutes())
-  const time = `${hours}:${minutes}`
+  const formatNumber = (number) => number.toString().padStart(2, "0");
+  const date = new Date(data.createdAt);
+  const hours = formatNumber(date.getHours());
+  const minutes = formatNumber(date.getMinutes());
+  const time = `${hours}:${minutes}`;
 
   useEffect(() => {
-    dispatch(getProfile(null, null, null))
-  }, [dispatch])
+    dispatch(getProfile(null, null, null));
+  }, [dispatch]);
 
-  const cardStyle = {
-    backgroundColor: data?.user?.id === user?.id ? "green" : "blue",
-    color: "white",
-  }
+  const isCurrentUser = data?.user?.id === user?.id;
+  const direction = isCurrentUser ? "outgoing" : "incoming";
+
   return (
-    <Card body style={cardStyle}>
-      <Image
-        src={data?.user?.photo}
-        className="rounded-circle"
-        width={50}
-        alt="photo"
-      />
-      <div>
-        {data?.message} by {data?.user?.name}
-      </div>
-      <div>{time}</div>
-    </Card>
-  )
+    <Message
+      model={{
+        message: data?.message,
+        sentTime: time,
+        sender: data?.user?.name,
+        direction: direction,
+        position: "single",
+      }}
+      >
+      <Avatar src={data?.user?.photo} name={data?.user?.name} />
+      <Message.Footer>{data?.user?.name} at { time } WIB</Message.Footer>
+    </Message>
+  );
 }
 
 MessageItem.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default MessageItem
+export default MessageItem;
